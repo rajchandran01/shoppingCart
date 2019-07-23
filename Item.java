84da1dbf72bf402be0234fe6e33f8e8ad07903f2
rajchandran01 <53204377+rajchandran01@users.noreply.github.com>
2019-07-23 00:07:46 -0400

    Add files via upload
    Item Class will hold the name and price of each inventory item

---
+++ Item.java
@@ -0,0 +1,30 @@
+package shoppingCart;
+
+public class Item {
+	
+	private String name;
+	private double price;
+	
+	public Item(String name, double price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getPrice() {
+		return this.price;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+	
+}
